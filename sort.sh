684616af38e9ef07f667f7ede90ee1238aae3f61
swarnenduisi <swar.prof@gmail.com>
2016-02-13 15:27:07 +0530

    updating script

--- sort.sh
+++ sort.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 cd ~
-cd /Users/swarnenduchakraborty/study/dissertation/
-sort -t " " -nk1 -r -nk3 final_score.txt >> final_score_sort.txt
+cd /Users/swarnenduchakraborty/study/dissertation/experiment_1/
+sort -t " " -nk1 -r -nk3 final_score.txt > final_score_sort.txt
+#cp result.txt result_plain.txt
+#cp result1.5_0.5.txt result_plain1.5_0.5.txt
